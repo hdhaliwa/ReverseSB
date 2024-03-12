7ca052757946325f911b7e5c74ddaa246d6d5725
hdhaliwa <hdhaliwa@asu.edu>
2024-03-12 10:10:36 -0700

    commit for code

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String input = "cake";
+
+        StringBuilder input1 = new StringBuilder();
+
+        input1.append(input);
+
+        input1.reverse();
+
+        System.out.print(input1);
+
+    }
+}
